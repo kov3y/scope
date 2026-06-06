84e9a1016a7c765e82b6e85db296d96cdcf9a35d
theking90000 <martin.cogh@gmail.com>
2026-06-06 15:17:07 +0200

    Update group ID and repository URLs in build.gradle.kts for consistency

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ val resolvedVersion = if (System.getenv("GITHUB_REF_TYPE") == "tag") {
 }
 
 allprojects {
-    group = "be.theking90000.di"
+    group = "be.theking90000"
     version = resolvedVersion
 
     repositories {
@@ -45,7 +45,7 @@ subprojects {
                 pom {
                     name = project.name
                     description = "Dependency injection utilities for Java."
-                    url = "https://github.com/theking90000/mclib3"
+                    url = "https://github.com/theking90000/scope"
 
                     licenses {
                         license {
@@ -62,9 +62,9 @@ subprojects {
                     }
 
                     scm {
-                        connection = "scm:git:git://github.com/theking90000/mclib3.git"
-                        developerConnection = "scm:git:ssh://github.com/theking90000/mclib3.git"
-                        url = "https://github.com/theking90000/mclib3"
+                        connection = "scm:git:git://github.com/theking90000/scope.git"
+                        developerConnection = "scm:git:ssh://github.com/theking90000/scope.git"
+                        url = "https://github.com/theking90000/scope"
                     }
                 }
             }
@@ -73,7 +73,7 @@ subprojects {
         repositories {
             maven {
                 name = "GitHubPackages"
-                url = uri("https://maven.pkg.github.com/theking90000/mclib3")
+                url = uri("https://maven.pkg.github.com/theking90000/scope")
                 credentials {
                     username = System.getenv("GITHUB_ACTOR")
                     password = System.getenv("GITHUB_TOKEN")
