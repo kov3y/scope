ab470c4e5218bc0dff190dc03fe8746d67955337
kov3y <ginzow.jett@gmail.com>
2026-06-26 20:12:33 -0400

    feat: add buildSrc module with Java conventions precompiled script plugins

---
+++ buildSrc/src/main/kotlin/be/theking90000/scope/java-base-conventions.gradle.kts
@@ -0,0 +1,9 @@
+package be.theking90000.scope
+
+plugins.withType<JavaPlugin> {
+  extensions.getByType<JavaPluginExtension>().apply {
+    toolchain {
+      languageVersion = JavaLanguageVersion.of(17)
+    }
+  }
+}
