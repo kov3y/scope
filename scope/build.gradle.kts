f3924a0d40d4f2846e4d38f1213a9228444c32a1
kov3y <ginzow.jett@gmail.com>
2026-06-26 20:20:03 -0400

    chore: move Maven publishing and test configuration to precompiled script plugins

--- scope/build.gradle.kts
+++ scope/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
   be.theking90000.scope.`java-library-conventions`
+  be.theking90000.scope.`maven-publishing-conventions`
 }
 
 dependencies {
@@ -7,4 +8,12 @@ dependencies {
   testImplementation(libs.junit.jupiter)
 
   testRuntimeOnly(libs.junit.platform.launcher)
+}
+
+tasks {
+  test {
+    configureEach {
+      useJUnitPlatform()
+    }
+  }
 }
